516d583e6fcf11450f23ddbc8b4234c59cca9e62
Ramiro Magno <ramiro.magno@gmail.com>
2022-05-31 23:29:01 +0100

    First commit

---
+++ R/example.R
@@ -0,0 +1,13 @@
+#' Just an example for Hadley
+#'
+#' Hi Hadley!
+#'
+#' ```{r plot, dev = 'svg',fig.asp=2/(1+sqrt(5))}
+#' plot(x = 1:10, y = (1:10)^2, type = 'l')
+#' ```
+#'
+#' @md
+#' @export
+example <- function() {
+
+}
